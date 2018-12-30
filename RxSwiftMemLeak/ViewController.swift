86cb990393838f4de384da78bec6ae5471e6ad69
Mikko Välimäki <mkko1373@gmail.com>
2018-12-30 12:58:48 +0200

    Project structure

---
+++ RxSwiftMemLeak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftMemLeak
+//
+//  Created by Mikko Välimäki on 2018-12-30.
+//  Copyright © 2018 Mikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
