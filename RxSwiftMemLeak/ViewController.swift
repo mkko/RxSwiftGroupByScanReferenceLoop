aa0eb7468a1c1f97327c11da818d508e77e809a4
Mikko Välimäki <mkko1373@gmail.com>
2018-12-30 13:38:03 +0200

    Introduce the issue

--- RxSwiftMemLeak/ViewController.swift
+++ RxSwiftMemLeak/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import RxSwift
+
+class IntListener {
+    let key: Int
+    private let disposeBag = DisposeBag()
+    init(key: Int, updates: Observable<Int>) {
+        self.key = key
+        updates
+            .debug("IntListener.updates(\(key))")
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
+
+    deinit {
+        print("---- Killed IntListener(\(key))")
+    }
+}
 
 class ViewController: UIViewController {
 
+    private var temp: Disposable!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let activeToggle = Observable<Int>
+            .interval(15, scheduler: MainScheduler.instance)
+            .startWith(0)
+            .map { $0 % 2 == 0}
+        let someProducer = Observable<Int>
+            .interval(1, scheduler: MainScheduler.instance)
+            .startWith(0)
+
+        temp = activeToggle
+            .flatMapLatest { isActive -> Observable<[IntListener]> in
+                if isActive {
+                    return someProducer
+                        .groupBy(keySelector: { _ in 0 })
+                        .scan([], accumulator: { (acc, g) in
+                            acc + [IntListener(key: g.key, updates: g.asObservable())]
+                        })
+                } else {
+                    return .just([])
+                }
+            }
+            .debug("-----")
+            .subscribe()
+
+
     }
 
 
